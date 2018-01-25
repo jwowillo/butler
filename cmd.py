4ee0239b13e1c431ad7476148913b44e2a49ad2c
jwowillo <will.orourke10@gmail.com>
2018-01-25 17:18:33 -0600

    Rewrite run Scripts in Python

---
+++ cmd.py
@@ -0,0 +1,22 @@
+"""cmd has helper functions for running shell commands."""
+
+import subprocess
+
+
+def ssh(host, cmd):
+    """ssh into host and run cmd."""
+    run('ssh {} << EOF\n{}\nEOF\n'.format(host, cmd))
+
+
+def run(cmd):
+    """run cmd in bash."""
+    pipe = subprocess.Popen('set -e\n{}\n'.format(cmd),
+                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                            shell=True)
+    for line in iter(pipe.stdout.readline, ''):
+        line = line.decode('utf-8')
+        if line == '': break
+        print(line)
+    pipe.stdout.close()
+    code = pipe.wait()
+    if code: raise subprocess.CalledProcessError(code, cmd)
